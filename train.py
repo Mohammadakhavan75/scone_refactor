ab16654f60fb6dc13fbdf7010f51f3fcd1ea80ca
mohammadakhavan75 <mohammad.akhavan75@gmail.com>
2023-11-13 08:40:02 +0000

    creating paths, adding for loop and save checkpoints

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
-import argparse
+from torch.utils.tensorboard import SummaryWriter
 from models.wrn import WideResNet
+from datetime import datetime
+import numpy as np
+import argparse
 import torch
+import os
+
 
 def parsing():
     parser = argparse.ArgumentParser(description='Tunes a CIFAR Classifier with OE',
@@ -48,10 +53,6 @@ def parsing():
                         help='dropout probability')
     
     # Checkpoints
-    parser.add_argument('--results_dir', type=str,
-                        default='results', help='Folder to save .pkl results.')
-    parser.add_argument('--checkpoints_dir', type=str,
-                        default='checkpoints', help='Folder to save .pt checkpoints.')
     parser.add_argument('--load_pretrained', type=str, default=None,
                          help='Load pretrained model to test or resume training.')
     parser.add_argument('--start_epoch', type=int, default=0,
@@ -98,6 +99,7 @@ def load_optim(args, model):
 
 if __name__ == "__main__":
     args = parsing()
+
     if args.mode == 'multiclass':
         num_classes = 10
     elif args.mode == 'oneclass':
@@ -112,3 +114,48 @@ if __name__ == "__main__":
     learnable_parameter_w = learnable_parameter_w.to(args.device)
     
     optimizer, scheduler = load_optim(args, model)
+
+    if args.load_pretrained is not None:
+        print("Loading from pretrained model...")
+        model.load_state_dict(torch.load(args.load_pretrained))
+        
+    #     save_path = args.save_path
+    #     model_save_path = save_path + 'models/'
+    # else:
+    addr = datetime.today().strftime('%Y_%m_%d_%H_%M_%S_%f')
+    save_path = './run/exp_' + addr + f'_{args.mode}' + f"__{args.run_index}__" + f'_{args.learning_rate}' + f'_{args.lr_update_rate}' + f'_{args.lr_gamma}' + f'_{args.optimizer}' + '/'
+    model_save_path = save_path + 'models/'
+    if not os.path.exists(model_save_path):
+        os.makedirs(model_save_path, exist_ok=True)
+
+    writer = SummaryWriter(save_path)
+    train_global_iter = 0
+    global_eval_iter = 0
+    best_acc = 0.0
+    
+    for epoch in range(0, args.epochs):
+        print('epoch', epoch + 1, '/', args.epochs)
+
+        train_global_iter, epoch_loss, epoch_accuracies = train(train_loader, out_train_loader, net, train_global_iter, criterion, optimizer, sigmoid, device)
+        global_eval_iter, eval_loss, eval_acc, eval_auc = test(val_loader, out_val_loader, net, global_eval_iter, criterion, device, sigmoid)
+
+
+        writer.add_scalar("Train/avg_loss", np.mean(epoch_loss), epoch)
+        writer.add_scalar("Train/avg_acc", np.mean(epoch_accuracies), epoch)
+        writer.add_scalar("Evaluation/avg_loss", np.mean(eval_loss), epoch)
+        writer.add_scalar("Evaluation/avg_acc", np.mean(eval_acc), epoch)
+        writer.add_scalar("Evaluation/avg_auc", np.mean(eval_auc), epoch)
+
+        print(f"Train/avg_loss: {np.mean(epoch_loss)} Train/avg_acc: {np.mean(epoch_accuracies)} \
+            Evaluation/avg_loss: {np.mean(eval_loss)} Evaluation/avg_acc: {np.mean(eval_acc)}  Evaluation/avg_auc: {np.mean(eval_auc)}")
+        
+        if (epoch+1) % 5 == 0:
+            torch.save(model.state_dict(), os.path.join(model_save_path, f'model_params_epoch_{epoch}.pt'))
+
+        if np.mean(eval_acc) > best_acc:
+            best_acc = np.mean(eval_acc)
+            torch.save(model.state_dict(), os.path.join(model_save_path, 'best_params.pt'))
+        
+        if np.mean(eval_acc) < best_acc:
+            scheduler.step()
+        
